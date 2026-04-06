63ef0612b60b0c472aebac9da5fce1d411281e6e
JuanJaramillo12004 <juan_edu.jaramillo@uao.edu.co>
2026-04-05 19:30:20 -0500

    feat: pokemon, auth, prisma, users modules/controllers/services that have been initialized

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UsersModule,
+    PrismaModule,
+    PokemonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
