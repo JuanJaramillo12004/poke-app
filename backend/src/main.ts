67145a47d5fb09d6751bc95a7d8614e47a0f368f
JuanJaramillo12004 <juan_edu.jaramillo@uao.edu.co>
2026-04-05 19:22:06 -0500

    feat: add user authentication and favorite Pokémon management
    - Updated package.json to include necessary dependencies for authentication and validation.
    - Implemented global validation pipe in main.ts for input validation.
    - Enabled CORS with configurable frontend URL.
    - Created Prisma schema for User and FavoritePokemon models to manage user data and their favorite Pokémon.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
+// Global Options for ValidationPipe
+const globalOptions = {
+  whitelist: true,
+  forbidNonWhitelisted: true,
+  transform: true,
+};
+
+// Bootstrap function to start the NestJS application
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with specific options
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+  });
+  app.useGlobalPipes(new ValidationPipe(globalOptions));
+
+  // Start the application on the specified port to 3000
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
